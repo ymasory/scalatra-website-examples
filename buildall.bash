f0589ffe9ba7308b64fc459dfaba7fbb0604d45a
Yuvi Masory <ymasory@gmail.com>
2012-12-25 02:26:16 -0500

    fix multiproj build

--- buildall.bash
+++ buildall.bash
@@ -4,11 +4,13 @@ set -e
 
 for SCRIPT in `find . -name sbt -type f -perm -u=x`
 do
+    OLD_PWD=`pwd`
     PROJ=`dirname $SCRIPT`
     echo '########################################################'
     echo "BUILDING $PROJ                                          "
     echo '########################################################'
     cd $PROJ
     ./sbt test
+    cd $OLD_PWD
 done
 
