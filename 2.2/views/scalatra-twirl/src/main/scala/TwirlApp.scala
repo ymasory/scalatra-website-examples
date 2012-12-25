2e55a761d2b7d1f885a90e6375b8051b8be5ab67
Yuvi Masory <ymasory@gmail.com>
2012-12-25 17:00:55 -0500

    simplify

--- 2.2/views/scalatra-twirl/src/main/scala/TwirlApp.scala
+++ 2.2/views/scalatra-twirl/src/main/scala/TwirlApp.scala
@@ -1,21 +1,10 @@
 package org.scalatra.example
 
 import org.scalatra._
-import scalate.ScalateSupport
 
-class TwirlApp extends ScalatraServlet with ScalateSupport {
+class TwirlApp extends ScalatraServlet {
 
   get("/") {
     html.helloTwirl.render(new java.util.Date)
-  }
-
-  notFound {
-    // remove content type in case it was set through an action
-    contentType = null
-    // Try to render a ScalateTemplate if no route matched
-    findTemplate(requestPath) map { path =>
-      contentType = "text/html"
-      layoutTemplate(path)
-    } orElse serveStaticResource() getOrElse resourceNotFound()
   }
 }
