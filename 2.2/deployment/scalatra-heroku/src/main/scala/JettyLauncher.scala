c62b1b7df5d765d255f3d208bf3b8ab3aae407ab
Yuvi Masory <ymasory@gmail.com>
2012-12-25 02:50:33 -0500

    fix package name

--- 2.2/deployment/scalatra-heroku/src/main/scala/JettyLauncher.scala
+++ 2.2/deployment/scalatra-heroku/src/main/scala/JettyLauncher.scala
@@ -10,7 +10,7 @@ object JettyLauncher {
     val context = new WebAppContext()
     context setContextPath "/"
     context.setResourceBase("src/main/webapp")
-    context.addServlet(classOf[com.example.app.MyScalatraServlet], "/*")
+    context.addServlet(classOf[org.scalatra.example.HerokuApp], "/*")
     context.addServlet(classOf[DefaultServlet], "/")
 
     server.setHandler(context)
