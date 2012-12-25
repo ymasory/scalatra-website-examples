0c0f90b0a9e35158251073d18ef58d10328e2329
Yuvi Masory <ymasory@gmail.com>
2012-12-25 02:46:53 -0500

    add jelastic example

---
+++ 2.2/deployment/scalatra-jelastic/project/plugins.sbt
@@ -0,0 +1,3 @@
+libraryDependencies += "com.github.siasia" %% "xsbt-web-plugin" % "0.12.0-0.2.11.1"
+
+addSbtPlugin("com.github.casualjim" % "sbt-jelastic-deploy" % "0.1.2")
---
+++ 2.2/deployment/scalatra-jelastic/src/main/scala/JelasticApp.scala
@@ -0,0 +1,25 @@
+package org.scalatra.example
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class JelasticApp extends ScalatraServlet with ScalateSupport {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>This is scalatra-jelastic!</h1>
+      </body>
+    </html>
+  }
+
+  notFound {
+    // remove content type in case it was set through an action
+    contentType = null
+    // Try to render a ScalateTemplate if no route matched
+    findTemplate(requestPath) map { path =>
+      contentType = "text/html"
+      layoutTemplate(path)
+    } orElse serveStaticResource() getOrElse resourceNotFound()
+  }
+}
