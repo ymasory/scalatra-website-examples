b9be4d3bf98f370e460468a74db812cefbd3a50c
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-01-17 11:14:43 +0000

    Renaming classfile and changing URL.

--- 2.2/async/akka-dispatch-example/src/main/scala/com/example/app/PageRetriever.scala
+++ 2.2/async/akka-dispatch-example/src/main/scala/com/example/app/PageRetriever.scala
@@ -13,14 +13,14 @@ object DispatchAkka {
 
   def retrievePage()(implicit ctx: ExecutionContext): Future[String] = {
     val prom = AkkaPromise[String]()
-    dispatch.Http(url("http://http://asofterworld.com/") OK as.String) onComplete {
+    dispatch.Http(url("http://slashdot.org/") OK as.String) onComplete {
       case r => prom.complete(r)
     }
     prom.future
   }
 }
 
-class MyScalatraServlet extends ScalatraServlet with AkkaSupport {
+class PageRetriever extends ScalatraServlet with AkkaSupport {
 
   implicit val system = ActorSystem()
   protected implicit def executor: ExecutionContext = system.dispatcher
