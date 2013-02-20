fe85bfb7254ffc5edbd88dfc8b48f226698e23bd
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-02-20 22:19:23 +0000

    Tests running clean.

--- 2.2/async/akka-examples/src/test/scala/com/example/app/MyActorAppSpec.scala
+++ 2.2/async/akka-examples/src/test/scala/com/example/app/MyActorAppSpec.scala
@@ -1,6 +1,7 @@
 package com.example.app
 
 import org.scalatra.test.specs2._
+import akka.actor.{Props, ActorSystem}
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class MyActorAppSpec extends ScalatraSpec { def is =
@@ -8,9 +9,13 @@ class MyActorAppSpec extends ScalatraSpec { def is =
     "should return status 200"                  ! async200^
                                                 end
 
-  addServlet(classOf[MyActorApp], "/*")
 
-  def async200 = get("/actors/async") {
+  val system = ActorSystem()
+  val myActor = system.actorOf(Props[MyActor])
+
+  addServlet(new MyActorApp(system, myActor), "/*")
+
+  def async200 = get("/async") {
     status must_== 200
   }
 }
