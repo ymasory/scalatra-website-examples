9d128ea083f0ab304fc5038a308063f3bd1cdfa8
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-02-21 18:33:03 +0000

    Updating to 2.2.0 final.

--- 2.2/http/authentication-demo/project/build.scala
+++ 2.2/http/authentication-demo/project/build.scala
@@ -1,60 +1,45 @@
 import sbt._
 import Keys._
+import org.scalatra.sbt._
+import org.scalatra.sbt.PluginKeys._
+import com.mojolly.scalate.ScalatePlugin._
+import ScalateKeys._
 
-object AuthenticationDemoBuild extends Build {
-  val Organization = "org.scalatra"
+object MyScalatraWebAppBuild extends Build {
+  val Organization = "com.example"
   val Name = "Authentication Demo"
   val Version = "0.1.0-SNAPSHOT"
   val ScalaVersion = "2.10.0"
-  val ScalatraVersion = "2.2.0-SNAPSHOT"
-
-  import java.net.URL
-  import com.github.siasia.PluginKeys.port
-  import com.github.siasia.WebPlugin.{container, webSettings}
+  val ScalatraVersion = "2.2.0"
 
   lazy val project = Project (
     "authentication-demo",
     file("."),
-    settings = Defaults.defaultSettings ++ webSettings ++ Seq(
+    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
       scalaVersion := ScalaVersion,
-      classpathTypes += "orbit",
       resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-      scalatraCrossVersion <<= (scalaVersion) {
-        case v if v startsWith "2.9." => CrossVersion.Disabled
-        case _ => CrossVersion.binary
-      },
-      libraryDependencies <<= (libraryDependencies, scalatraCrossVersion) {
-        (libs, scalatraCV) => libs ++ Seq(
-          "org.scalatra" % "scalatra" % ScalatraVersion cross scalatraCV,
-          "org.scalatra" % "scalatra-auth" % ScalatraVersion cross scalatraCV,
-          "org.scalatra" % "scalatra-scalate" % ScalatraVersion cross scalatraCV,
-          "org.scalatra" % "scalatra-specs2" % ScalatraVersion % "test" cross scalatraCV,
-          "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-          "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "container",
-          "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+      libraryDependencies ++= Seq(
+        "org.scalatra" % "scalatra-auth" % ScalatraVersion,
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+      ),
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty,  /* default imports should be added here */
+            Seq.empty,  /* add extra bindings here */
+            Some("templates")
+          )
         )
-      },
-      browseTask
+      }
     )
   )
-
-  val scalatraCrossVersion = SettingKey[CrossVersion]("scalatra-cross-version", "cross build strategy for Scalatra")
-
-  val browse = TaskKey[Unit]("browse", "open web browser to localhost on container:port")
-  val browseTask = browse <<= (streams, port in container.Configuration) map { (streams, port) =>
-    import streams.log
-    val url = new URL("http://localhost:%s" format port)
-    try {
-      log info "Launching browser."
-      java.awt.Desktop.getDesktop.browse(url.toURI)
-    }
-    catch {
-      case _ => {
-        log info { "Could not open browser, sorry. Open manually to %s." format url.toExternalForm }
-      }
-    }
-  }
 }
