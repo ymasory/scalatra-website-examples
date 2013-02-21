4071be42ac7d08fd38ec28018f5dee8b4ed87747
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-02-21 18:27:10 +0000

    Setting Jelastic environment vars to make the tests pass.

--- buildall.bash
+++ buildall.bash
@@ -2,6 +2,11 @@
 
 set -e
 
+# These are necessary in order to test the deployment/scala-jelastic project.
+export JELASTIC_USERNAME="you@you.com"
+export JELASTIC_PWD="iluvyou"
+
+# Run the test suite for each project.
 for SCRIPT in `find . -name sbt -type f -perm -u=x`
 do
     OLD_PWD=`pwd`
