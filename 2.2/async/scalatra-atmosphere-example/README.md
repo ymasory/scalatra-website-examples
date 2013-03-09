a99df8739786b22ba0ce0ccbe1fbe8e04cba3b74
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-03-09 20:40:29 +0000

    Adding an Atmosphere example.

---
+++ 2.2/async/scalatra-atmosphere-example/README.md
@@ -0,0 +1,15 @@
+# Scalatra Atmosphere Example #
+
+A quick example app showing the new Atmosphere support in 
+[Scalatra](http://scalatra.org) 2.2.
+
+## Build & Run ##
+
+```sh
+$ cd $name__snake$
+$ ./sbt
+> container:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ 2.2/async/scalatra-atmosphere-example/project/build.scala
@@ -0,0 +1,48 @@
+import sbt._
+import Keys._
+import org.scalatra.sbt._
+import org.scalatra.sbt.PluginKeys._
+import com.mojolly.scalate.ScalatePlugin._
+import ScalateKeys._
+
+object AtmosphereBuild extends Build {
+  val Organization = "com.example"
+  val Name = "Scalatra Atmosphere Example"
+  val Version = "0.1.0-SNAPSHOT"
+  val ScalaVersion = "2.9.2"
+  val ScalatraVersion = "2.2.0"
+
+  lazy val project = Project (
+    "atmosphere-example",
+    file("."),
+    settings =  Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      resolvers += "Akka Repo" at "http://repo.akka.io/repository",
+      libraryDependencies ++= Seq(
+        "org.json4s"   %% "json4s-jackson" % "3.1.0",
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "org.scalatra" %% "scalatra-atmosphere" % ScalatraVersion,
+        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty" % "jetty-websocket" % "8.1.8.v20121106" % "container;provided",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+      ),
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty,  /* default imports should be added here */
+            Seq.empty,  /* add extra bindings here */
+            Some("templates")
+          )
+        )
+      }
+    )
+  )
+}
