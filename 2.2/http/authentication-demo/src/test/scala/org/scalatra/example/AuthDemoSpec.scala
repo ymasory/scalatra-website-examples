c9700b1cd1efb8d2237ba6e3dd9ef688e90612e3
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-03-09 13:49:58 +0000

    Fixed AuthDemoSpec, root returns a 401 on auth check.

--- 2.2/http/authentication-demo/src/test/scala/org/scalatra/example/AuthDemoSpec.scala
+++ 2.2/http/authentication-demo/src/test/scala/org/scalatra/example/AuthDemoSpec.scala
@@ -5,12 +5,12 @@ import org.scalatra.test.specs2._
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class AuthDemoSpec extends ScalatraSpec { def is =
   "GET / on AuthDemo"                     ^
-    "should return status 200"                  ! root200^
+    "should return status 401"                  ! root401^
                                                 end
 
   addServlet(classOf[AuthDemo], "/*")
 
-  def root200 = get("/") {
-    status must_== 200
+  def root401 = get("/") {
+    status must_== 401
   }
 }
