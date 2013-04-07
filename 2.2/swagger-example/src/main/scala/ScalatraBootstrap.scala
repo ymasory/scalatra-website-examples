6ae5aad00b8fbc33712ca1e7ed1ccee820808018
futurechimp <dave.hrycyszyn@headlondon.com>
2013-04-07 16:56:05 +0100

    Removing unused logger statements.

--- 2.2/swagger-example/src/main/scala/ScalatraBootstrap.scala
+++ 2.2/swagger-example/src/main/scala/ScalatraBootstrap.scala
@@ -2,13 +2,9 @@ import org.scalatra.example.swagger._
 import org.scalatra.LifeCycle
 import javax.servlet.ServletContext
 
-import org.slf4j.{Logger, LoggerFactory}
-
 class ScalatraBootstrap extends LifeCycle {
 
   implicit val swagger = new FlowersSwagger
-
-  val logger =  LoggerFactory.getLogger(getClass)
 
   override def init(context: ServletContext) {
     try {
