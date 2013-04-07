d7a35c2274417cb53858079df9973d8fcb3d5734
futurechimp <dave.hrycyszyn@headlondon.com>
2013-04-07 16:54:51 +0100

    Try/catch to winkle out exception on startup.

--- 2.2/swagger-example/src/main/scala/ScalatraBootstrap.scala
+++ 2.2/swagger-example/src/main/scala/ScalatraBootstrap.scala
@@ -2,12 +2,20 @@ import org.scalatra.example.swagger._
 import org.scalatra.LifeCycle
 import javax.servlet.ServletContext
 
+import org.slf4j.{Logger, LoggerFactory}
+
 class ScalatraBootstrap extends LifeCycle {
 
   implicit val swagger = new FlowersSwagger
 
+  val logger =  LoggerFactory.getLogger(getClass)
+
   override def init(context: ServletContext) {
-    context.mount(new FlowersController, "/flowers/*")
-    context.mount (new ResourcesApp, "/api-docs/*")
+    try {
+      context.mount(new FlowersController, "/flowers/*")
+      context.mount (new ResourcesApp, "/api-docs/*")
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
   }
 }
