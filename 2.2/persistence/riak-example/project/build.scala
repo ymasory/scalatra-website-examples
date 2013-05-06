edc3b403ec205745cacd91dae17fbebca5ab7360
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-05-06 11:35:54 +0100

    Added no-assertion tests, just testing for compilation failure.
    This way whoever's testing the suite doesn't need to have Riak running.

--- 2.2/persistence/riak-example/project/build.scala
+++ 2.2/persistence/riak-example/project/build.scala
@@ -25,7 +25,7 @@ object RiakExampleBuild extends Build {
         "com.basho.riak" % "riak-client" % "1.1.0",
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
