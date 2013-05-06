e75149e2aeb8f2dbba510bb2c8864232e1e2dea1
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-05-06 01:44:53 +0100

    Some docs in the bootstrap file.

--- 2.2/persistence/scalatra-casbah-example/src/main/scala/ScalatraBootstrap.scala
+++ 2.2/persistence/scalatra-casbah-example/src/main/scala/ScalatraBootstrap.scala
@@ -5,10 +5,14 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    // Connect to default - localhost, 27017
+
+     // As you can see, there's not much to do in order to get MongoDb working with Scalatra.
+     // We're connecting with default settings - localhost on port 27017 - by calling MongoClient() with no arguments.
     val mongoClient =  MongoClient()
     val mongoColl = mongoClient("casbah_test")("test_data")
 
+    // pass a reference to the Mongo collection into your servlet when you mount it at application start:
     context.mount(new MongoController(mongoColl), "/*")
+
   }
 }
