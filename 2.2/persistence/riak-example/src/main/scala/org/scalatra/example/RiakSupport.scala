6120010476fe6579590b1b4cc05593e0880e0c04
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-05-06 11:39:53 +0100

    Added some doc comments on examples.

--- 2.2/persistence/riak-example/src/main/scala/org/scalatra/example/RiakSupport.scala
+++ 2.2/persistence/riak-example/src/main/scala/org/scalatra/example/RiakSupport.scala
@@ -3,6 +3,10 @@ package org.scalatra.example
 import com.basho.riak.client.RiakFactory
 import org.slf4j.LoggerFactory
 
+/**
+ * A trait we mix into Scalatra's initalization lifecycle to ensure we've got
+ * a Riak client and a bucket set up after the application starts.
+ */
 trait RiakJavaClientInit {
   val logger = LoggerFactory.getLogger(getClass)
 
@@ -25,6 +29,13 @@ trait RiakJavaClientInit {
 
 }
 
+/**
+ * A trait we can use to get a handle on the Riak bucket we created at
+ * application start.
+ *
+ * The big questions here: does this need to be clustered? What are the implications
+ * for thread-safety - do we need to be prefixing operations with a clientId?
+ */
 trait RiakSupport {
 
   def myBucket = {
