fba94696aab56a3aab527aaae5bdb48c714f9252
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-08-25 21:42:39 +0200

    Added comments.

--- 2.2/persistence/scalatra-casbah-example/src/main/scala/org/scalatra/example/MongoController.scala
+++ 2.2/persistence/scalatra-casbah-example/src/main/scala/org/scalatra/example/MongoController.scala
@@ -1,11 +1,16 @@
 package org.scalatra.example
 
 import org.scalatra._
+import org.scalatra.json.JacksonJsonSupport
+import org.json4s._
+import org.json4s.mongo.{JObjectParser, ObjectIdSerializer}
 
 // MongoDb-specific imports
 import com.mongodb.casbah.Imports._
 
-class MongoController(mongoColl: MongoCollection) extends ScalatraCasbahExampleStack {
+class MongoController(mongoColl: MongoCollection)
+  extends ScalatraCasbahExampleStack
+  with JacksonJsonSupport /* this is optional and used by the json4s sample at the end */ {
 
   /**
    * Insert a new object into the database. You can use the following from your console to try it out:
@@ -34,5 +39,38 @@ class MongoController(mongoColl: MongoCollection) extends ScalatraCasbahExampleS
     val q = MongoDBObject(params("key") -> params("value"))
     for ( x <- mongoColl.findOne(q) ) yield x
   }
-  
+
+  // this is an approach to returning MongoDB results as JSON using json4s
+  implicit val jsonFormats = DefaultFormats + new ObjectIdSerializer
+
+  get("/asJson") {
+    mongoColl.find
+  }
+
+  get("/asJson/:key/:value") {
+    val q = MongoDBObject(params("key") -> params("value"))
+    mongoColl.findOne(q) match {
+      case Some(x) => x
+      case None => halt(404)
+    }
+  }
+
+  // convert MongoDB results to JValue
+  override protected def renderPipeline = ({
+    // handle DBObject
+    case dbo: DBObject => JObjectParser.serialize(dbo)
+
+    // handle MongoCursor
+    case xs: TraversableOnce[_] => JArray(xs.toList.map { x => JObjectParser.serialize(x) })
+
+    // handle Option[DBObject]
+    case Some(dbo: DBObject) => JObjectParser.serialize(dbo)
+    case None => JNothing
+  }: RenderPipeline) orElse super.renderPipeline
+
+  notFound {
+    // remove content type in case it was set through an action
+    contentType = null
+  }
+
 }
