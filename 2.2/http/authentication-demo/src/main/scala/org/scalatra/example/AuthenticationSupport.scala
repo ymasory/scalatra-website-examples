21783fd8074c94ab4a9686226b7b66b8d61f8cf4
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-10-19 14:00:45 +0100

    Adding implicits needed for Scalatra 2.2.1.

--- 2.2/http/authentication-demo/src/main/scala/org/scalatra/example/AuthenticationSupport.scala
+++ 2.2/http/authentication-demo/src/main/scala/org/scalatra/example/AuthenticationSupport.scala
@@ -3,17 +3,18 @@ package org.scalatra.example
 import org.scalatra.auth.strategy.{BasicAuthStrategy, BasicAuthSupport}
 import org.scalatra.auth.{ScentrySupport, ScentryConfig}
 import org.scalatra.{ScalatraBase}
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 
 
 class OurBasicAuthStrategy(protected override val app: ScalatraBase, realm: String)
   extends BasicAuthStrategy[User](app, realm) {
 
-  protected def validate(userName: String, password: String): Option[User] = {
+  protected def validate(userName: String, password: String)(implicit request: HttpServletRequest, response: HttpServletResponse): Option[User] = {
     if(userName == "scalatra" && password == "scalatra") Some(User("scalatra"))
     else None
   }
 
-  protected def getUserId(user: User): String = user.id
+  protected def getUserId(user: User)(implicit request: HttpServletRequest, response: HttpServletResponse): String = user.id
 }
 
 trait AuthenticationSupport extends ScentrySupport[User] with BasicAuthSupport[User] {
