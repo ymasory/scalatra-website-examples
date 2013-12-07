524e4e0ca85720215f182f57707bac4df67728f4
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-12-07 14:31:48 +0000

    Fixed ProtectedServletSpec.

--- 2.2/http/scentry-auth-demo/src/test/scala/com/constructiveproof/example/ProtectedServletSpec.scala
+++ 2.2/http/scentry-auth-demo/src/test/scala/com/constructiveproof/example/ProtectedServletSpec.scala
@@ -5,12 +5,12 @@ import org.scalatra.test.specs2._
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class ProtectedServletSpec extends ScalatraSpec { def is =
   "GET / on ProtectedServlet"                     ^
-    "should return status 200"                  ! root200^
+    "should return status 200"                  ! root302^
                                                 end
 
   addServlet(classOf[ProtectedController], "/*")
 
-  def root200 = get("/") {
-    status must_== 200
+  def root302 = get("/") {
+    status must_== 302
   }
 }
