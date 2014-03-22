d8847c392190b20952d0470445de6d8958349746
Răzvan Panda <razvan.panda@gmail.com>
2014-03-22 17:32:29 +0200

    Atmosphere 2.3 update

--- 2.3/async/scalatra-atmosphere-embedded/project/build.scala
+++ 2.3/async/scalatra-atmosphere-embedded/project/build.scala
@@ -1,17 +1,15 @@
 import sbt._
 import Keys._
-import org.scalatra.sbt._
-import org.scalatra.sbt.PluginKeys._
 import com.mojolly.scalate.ScalatePlugin._
 import ScalateKeys._
 import com.typesafe.sbt.SbtStartScript
 
-object AtmosphereBuild extends Build {
+object build extends Build {
   val Organization = "com.example"
   val Name = "Scalatra Atmosphere Embedded"
   val Version = "0.1.0-SNAPSHOT"
   val ScalaVersion = "2.10.3"
-  val ScalatraVersion = "2.3.0-SNAPSHOT"
+  val ScalatraVersion = "2.3.0.RC1"
   val jettyVersion = "9.1.3.v20140225"
 
   lazy val project = Project (
@@ -25,16 +23,16 @@ object AtmosphereBuild extends Build {
       resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
       resolvers += "Akka Repo" at "http://repo.akka.io/repository",
       libraryDependencies ++= Seq(
-        "org.json4s"   %% "json4s-jackson" % "3.1.0",
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "org.scalatra" %% "scalatra-atmosphere" % ScalatraVersion,
-        "ch.qos.logback"              %  "logback-classic"     % "1.1.1"          % "runtime",
-        "org.eclipse.jetty"           %  "jetty-plus"          % jettyVersion     % "runtime;provided",
-        "org.eclipse.jetty"           %  "jetty-webapp"        % jettyVersion     % "runtime",
-        "org.eclipse.jetty.websocket"  %  "websocket-server"        % "9.1.3.v20140225"     % "runtime;provided",
-        "javax.servlet"               %  "javax.servlet-api"   % "3.1.0"          % "runtime;compile;provided;test" artifacts Artifact("javax.servlet-api", "jar", "jar")
+        "org.json4s"                  %% "json4s-jackson"      % "3.1.0",
+        "org.scalatra"                %% "scalatra"            % ScalatraVersion,
+        "org.scalatra"                %% "scalatra-scalate"    % ScalatraVersion,
+        "org.scalatra"                %% "scalatra-specs2"     % ScalatraVersion   % "test",
+        "org.scalatra"                %% "scalatra-atmosphere" % ScalatraVersion,
+        "ch.qos.logback"              %  "logback-classic"     % "1.1.1"           % "runtime",
+        "org.eclipse.jetty"           %  "jetty-plus"          % jettyVersion      % "compile;provided",
+        "org.eclipse.jetty"           %  "jetty-webapp"        % jettyVersion      % "compile",
+        "org.eclipse.jetty.websocket" %  "websocket-server"    % "9.1.3.v20140225" % "compile;provided",
+        "javax.servlet"               %  "javax.servlet-api"   % "3.1.0"           % "compile;provided;test"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
