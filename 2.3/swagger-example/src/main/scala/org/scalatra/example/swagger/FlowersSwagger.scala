c483b782dd4f877e244468adecd4a85efe6aa634
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-07-24 00:21:55 -0700

    fix swagger example for info serialization

--- 2.3/swagger-example/src/main/scala/org/scalatra/example/swagger/FlowersSwagger.scala
+++ 2.3/swagger-example/src/main/scala/org/scalatra/example/swagger/FlowersSwagger.scala
@@ -6,12 +6,13 @@ import org.scalatra.swagger.{ApiInfo, NativeSwaggerBase, Swagger}
 
 class ResourcesApp(implicit val swagger: Swagger) extends ScalatraServlet with NativeSwaggerBase
 
-object FlowersApiInfo extends ApiInfo(
+object FlowersSwagger{
+  val Info = ApiInfo(
     "The Flowershop API",
     "Docs for the Flowers API",
     "http://scalatra.org",
     "apiteam@scalatra.org",
     "MIT",
     "http://opensource.org/licenses/MIT")
-
-class FlowersSwagger extends Swagger(Swagger.SpecVersion, "1.0.0", FlowersApiInfo)
+}
+class FlowersSwagger extends Swagger(Swagger.SpecVersion, "1", FlowersSwagger.Info)
