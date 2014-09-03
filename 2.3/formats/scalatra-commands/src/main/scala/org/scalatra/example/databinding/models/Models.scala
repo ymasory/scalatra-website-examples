2d11b1ca98b555082565ca00de89d723a88b428e
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-09-03 17:08:05 +0100

    Fixing Int type.

--- 2.3/formats/scalatra-commands/src/main/scala/org/scalatra/example/databinding/models/Models.scala
+++ 2.3/formats/scalatra-commands/src/main/scala/org/scalatra/example/databinding/models/Models.scala
@@ -1,4 +1,4 @@
 package org.scalatra.example.commands.models
 
 /** A Todo object to use as a data model */
-case class Todo(id: Integer, name: String, done: Boolean = false)
+case class Todo(id: Int, name: String, done: Boolean = false)
