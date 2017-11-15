015d5dd207ec8da2eea8704f16cfbf5e7ada5caa
Naoki Takezoe <takezoe@gmail.com>
2017-11-15 16:58:42 +0900

    Add CORS setting to the swagger example

--- 2.5/swagger-example/src/main/scala/ScalatraBootstrap.scala
+++ 2.5/swagger-example/src/main/scala/ScalatraBootstrap.scala
@@ -7,6 +7,7 @@ class ScalatraBootstrap extends LifeCycle {
   implicit val swagger = new FlowersSwagger
 
   override def init(context: ServletContext) {
+    context.initParameters("org.scalatra.cors.allowedOrigins") = "http://petstore.swagger.io"
     context.mount(new FlowersController, "/flowers", "flowers")
     context.mount (new ResourcesApp, "/api-docs")
   }
