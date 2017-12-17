ab90e63afb26542bb0ac72eda1e88ee8420deeba
Magnolia K <magnolia.k@icloud.com>
2017-12-18 00:43:01 +0900

    updated swagger-example
    - updated scala version & sbt version
    - updated dependent artifacts & plugins
    - updated the way of writing the test to the latest specs 2 notation

--- 2.6/swagger-example/src/test/scala/org/scalatra/example/swagger/FlowersSpec.scala
+++ 2.6/swagger-example/src/test/scala/org/scalatra/example/swagger/FlowersSpec.scala
@@ -2,17 +2,14 @@ package org.scalatra.example.swagger
 
 import org.scalatra.test.specs2._
 
-// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class FlowersSpec extends ScalatraSpec { def is =
-  "GET / on Flowers"                     ^
-    "should return status 200"                  ! root200^
-                                                p^
-  "GET / on Flowers with a name param"          ^
-    "should return status 200"                  ! nameParamWorks^
-                                                p^
-  "GET /:slug on Flowers"                       ^
-    "should return status 200"                  ! slugWorks
-                                                end
+class FlowersSpec extends ScalatraSpec { def is = s2"""
+  GET / on Flowers 
+    should return status 200 $root200
+  GET / on Flowers with a name param
+    should return status 200 $nameParamWorks
+  GET /:slug on Flowers
+    should return status 200 $slugWorks
+"""
 
   implicit val swagger = new FlowersSwagger
   addServlet(new FlowersController, "/flowers/*")
