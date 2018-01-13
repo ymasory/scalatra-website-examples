f0a8e750d3eb2e10dd7e94474535f31ddd7b7722
Magnolia K <magnolia.k@icloud.com>
2018-01-13 23:09:38 +0900

    Corrected according to the latest directory structure

--- buildall.bash
+++ buildall.bash
@@ -4,10 +4,6 @@
 
 set -e
 
-# These are necessary in order to test the deployment/scala-jelastic project.
-export JELASTIC_USERNAME="you@you.com"
-export JELASTIC_PWD="iluvyou"
-
 # Run the test suite for each project.
 for SCRIPT in `find . -name sbt -type f -perm -u=x`
 do
